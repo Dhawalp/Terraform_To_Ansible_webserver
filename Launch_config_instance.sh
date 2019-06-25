59ab6b3655d330ab2f6c431c2000d137adb24742
Dhawal <40303919+Dhawalp@users.noreply.github.com>
2019-06-24 20:02:03 -0400

    Add files via upload

---
+++ Launch_config_instance.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd terraTOansible/terracreate/
+terraform plan -out out.detailRE
+echo "I was here"
+sleep 5
+if [[ -e out.details ]];  then
+terraform apply out.detailRE
+sleep 15
+ipadd="$(terraform output instance_ip_addr)"
+
+/bin/cat <<EOM > /home/legendadmin/terraTOansible/hosts-device.yml
+# hosts-dev
+
+[webservers]
+app1 ansible_host=$ipadd
+
+[local]
+control ansible_connection=local
+
+EOM
+
+fi
+sleep 5
+cd ..
+ansible-playbook /home/legendadmin/terraTOansible/playbook/all-playbook.yml
